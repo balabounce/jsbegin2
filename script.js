5e0492775700433c65948935036bdb09e2b3d235
Balabounce <yoba.number.one@gmail.com>
2020-11-30 16:37:36 +0300

    third commit

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ let personaMovieDb = {
     genres: [],
     privat: false
 };
+//third 
+let lastMovieName = prompt("Один из последних просмотренных фильмов?", 'Терминатор');
+let rateMovie =  prompt("Насколько вы его оцениваете?", '10');
+
+personaMovieDb.movies[lastMovieName] = +rateMovie;
+
+console.log(personaMovieDb);
