0f4dae92ee556bcd3572a8f56e2af716f454b354
Balabounce <yoba.number.one@gmail.com>
2020-11-30 16:23:23 +0300

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 "use strict";
 //first
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', 10 );
-console.log(numberOfFilms);
+//second
+let personaMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
